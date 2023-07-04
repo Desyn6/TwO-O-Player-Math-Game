883837cfb9c7bc85dbbcd18de3e46a5c2516950e
Denny Ng <Dennyng1989@hotmail.com>
2023-07-03 18:12:14 -0700

    Impl. questions class/module

---
+++ question.rb
@@ -0,0 +1,33 @@
+class Question
+  # random number generator module
+  def random_number
+    rand(1..20)
+  end
+
+  # Asks and compares question
+  def ask_question
+    num_one = random_number
+    num_two = random_number
+    answer = num_one + num_two
+    puts "What is the sume of #{num_one} and #{num_two}?"
+    
+    # Get user response
+    response = nil
+
+    # Error handling for all non-number input
+    while (response.to_i == 0)
+      response = gets.chomp.to_i
+      
+      if response.to_i == 0 
+        puts "Please give me a non-zero number. Try again."
+      end
+    end
+
+    # return true or false
+    response == answer ? true : false
+  end
+end
+
+# Test questions Class + module
+# q = Question.new
+# puts q.ask_question
